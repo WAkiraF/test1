158db6141e165fcae79d408d7b238ec49fce0e72
WAkiraF <114143851+WAkiraF@users.noreply.github.com>
2023-05-12 11:09:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ new Vue({
 
 import axios from 'axios';
 
-const axios = require('axios');
+//const axios = require('axios');
 // ElasticsearchのAPIエンドポイントを指定
 const url = 'https://i-o-optimized-deployment-ad278b.es.ap-northeast-1.aws.found.io:9243';
 
