e8d811e603af6f092e1971353e5ea304757c510f
WAkiraF <114143851+WAkiraF@users.noreply.github.com>
2023-05-12 10:50:12 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+new Vue({
+    el:'#app',
+    data(){
+        return{
+            text:"hello world!"
+        };
+    }
+})
+
+import axios from 'axios';
+
+const axios = require('axios');
+// ElasticsearchのAPIエンドポイントを指定
+const url = 'https://i-o-optimized-deployment-ad278b.es.ap-northeast-1.aws.found.io:9243';
+
+    
+export default {
+  mounted(){
+
+	// Elasticsearchからデータを取得するためのHTTP GETリクエストを送信
+	axios.get(url)
+		.then((response) => {
+	    // レスポンスのデータを取得
+			const data = response.data;
+	    	console.log(data);
+  		})
+		.catch((error) => {
+    		console.log(error);
+	  	});
+
+  }
+}
